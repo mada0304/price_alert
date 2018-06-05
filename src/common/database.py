2acade52e9da28a43fbf274886289a59ae878183
mada0304 <mqda0304@gmail.com>
2018-06-05 13:19:57 -0700

    change the database collection

--- src/common/database.py
+++ src/common/database.py
@@ -3,13 +3,13 @@ import os
 
 
 class Database(object):
-    URI = os.environ.get("MONGOLAB_URI")
+    URI = os.environ.get("MONGOLAB)
     DATABASE = None
 
     @staticmethod
     def initialize():
         client = pymongo.MongoClient(Database.URI)
-        Database.DATABASE = client.get_default_database()
+        Database.DATABASE = client['heroku_132mgvg4']
 
     @staticmethod
     def insert(collection, data):
