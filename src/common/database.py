ffeea7c001d7d7e442423b45502bf00a849465db
mada0304 <mqda0304@gmail.com>
2018-06-05 13:35:41 -0700

    correct typo

--- src/common/database.py
+++ src/common/database.py
@@ -3,7 +3,7 @@ import os
 
 
 class Database(object):
-    URI = os.environ.get("mongodb://mqda0304@gmail.com:25257758tianyou@ds149960.mlab.com:49960")
+    URI = os.environ.get("MONGOLAB")
     DATABASE = None
 
     @staticmethod
